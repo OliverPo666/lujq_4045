52b124854b176baa94a9a0a85916094291cc3d99
OliverPo666 <lujq@mail.uc.edu>
2021-09-25 19:01:50 -0400

    create html file

--- src/main/java/com/lujq/lujq_Assignment0/lujqController.java
+++ src/main/java/com/lujq/lujq_Assignment0/lujqController.java
@@ -1,8 +1,14 @@
 package com.lujq.lujq_Assignment0;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class lujqController {
-
+	
+	@RequestMapping("/start")
+	public String start() {
+		return "start";
+	}
+	
 }
